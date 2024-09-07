d14285735344ebe789a41ad8039a3b486a4d8ad7
Nick <55831368+nickspb-ui@users.noreply.github.com>
2024-09-07 19:06:27 +0300

    Add files via upload

---
+++ Консольный ввод – вывод/1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+ 
+	public static void main (String [] args ){      
+        Scanner scan = new Scanner(System.in, "Cp866");
+        String inputName = scan.next();
+        scan.close();
+
+        System.out.println("Привет, " + inputName);
+	}
+}
