3355a7c509d1f261301c36f5d0b22215eb615914
Nick <55831368+nickspb-ui@users.noreply.github.com>
2024-09-07 20:13:45 +0300

    fixed tabs

--- Консольный ввод – вывод/1.java
+++ Консольный ввод – вывод/1.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
 
 class Main{
- 
 	public static void main (String [] args ){      
-        Scanner scan = new Scanner(System.in, "Cp866");
-        String inputName = scan.next();
-        scan.close();
-
-        System.out.println("Привет, " + inputName);
+        	Scanner scan = new Scanner(System.in, "Cp866");
+        	String inputName = scan.next();
+        	scan.close();
+       		System.out.println("Привет, " + inputName);
 	}
 }
