923b24428088817bfa6eaa31dd9f7d22fde1dbdc
Nick <55831368+nickspb-ui@users.noreply.github.com>
2024-09-08 09:58:34 +0300

    Add files via upload

---
+++ Документирование кода/1.java
@@ -0,0 +1,18 @@
+/**
+ * Класс видеоплеера
+ * 
+ * @author Николай Устинов
+ * @version 1.0
+ */
+class VideoPlayer {
+    /**
+     * Функция - изменение позиции воспроизведения видео
+     * 
+     * @param newPlaybackPoint - новая позиция
+     * @return возвращает логическое значение утверждения "позиция успешно изменена"
+     */
+    public boolean changePlaybackPoint(int newPlaybackPoint) {
+        // Some logic
+        return true;
+    }
+}
